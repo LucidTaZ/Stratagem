fee8acc1014d74f803304b9387939d91b29badc5
Thijs Zumbrink <tazaranie@gmail.com>
2016-07-30 20:54:25 +0200

    Add Heart building and respawn feature

---
+++ Assets/Scripts/General/AttachCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AttachCamera : MonoBehaviour {
+	public Transform AttachmentPoint;
+
+	CameraBehavior cb;
+
+	bool quitting = false;
+
+	void Start () {
+		cb = Camera.main.GetComponent<CameraBehavior>();
+		Debug.Assert(cb != null);
+		cb.AttachTo(AttachmentPoint);
+	}
+
+	void OnApplicationQuit () {
+		quitting = true;
+	}
+
+	void OnDestroy () {
+		// Also gets called on application quit.
+		if (!quitting) {
+			detach();
+		}
+	}
+	void detach () {
+		cb.Detach();
+	}
+}
---
+++ Assets/Scripts/General/CameraBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+// This script must be placed before the FirstPersonController!
+using System.Collections;
+
+
+public class CameraBehavior : MonoBehaviour {
+	public float ResetTime = 1f;
+
+	Vector3 initialPosition;
+	Quaternion initialRotation;
+
+	void Start () {
+		initialPosition = transform.position;
+		initialRotation = transform.rotation;
+	}
+
+	public void AttachTo (Transform attachmentPoint) {
+		transform.parent = attachmentPoint;
+		transform.localPosition = Vector3.zero;
+		transform.localRotation = Quaternion.identity;
+		transform.localScale = new Vector3(1, 1, 1);
+	}
+
+	public void Detach () {
+		transform.parent = null;
+		StartCoroutine(performSweep(initialPosition, initialRotation));
+	}
+
+	IEnumerator performSweep (Vector3 position, Quaternion rotation) {
+		Vector3 startPosition = transform.position;
+		Quaternion startRotation = transform.rotation;
+
+		for (float t = 0; t < ResetTime; t += Time.deltaTime) {
+			float smoothT = Mathf.SmoothStep(0, 1, t / ResetTime);
+			transform.position = Vector3.Lerp(startPosition, position, smoothT);
+			transform.rotation = Quaternion.Slerp(startRotation, rotation, smoothT);
+			yield return 0;
+		}
+	}
+}
