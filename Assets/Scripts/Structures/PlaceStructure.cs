32c9d5665c23ebcd146a7f4bcb4d7209477bc81d
Thijs Zumbrink <tazaranie@gmail.com>
2016-07-30 19:17:45 +0200

    Add structure placement feature

---
+++ Assets/Scripts/Structures/PlaceStructure.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class PlaceStructure : MonoBehaviour {
+	public GameObject Subject;
+
+	Material BlueprintMaterial;
+
+	BelongsToTeam btt;
+
+	const float MAX_DISTANCE = 5f;
+	const float MIN_NORMAL_ALIGNMENT = 0.9f; // Minimum dot product of surface normal and "up"
+
+	void Start () {
+		btt = GetComponent<BelongsToTeam>();
+		Debug.Assert(btt != null);
+		Debug.Assert(Subject != null);
+
+		BlueprintMaterial = Resources.Load<Material>("BlueprintMaterial"); // So we don't have to assign it all the time upon instantiation, but can still leverage the editor to design the material
+	}
+
+	void Update () {
+		Vector3 position;
+		Quaternion rotation;
+		if (canBuild(out position, out rotation)) {
+			if (Input.GetButton("Build")) {
+				buildStructure(position, rotation);
+				Destroy(this); // Destroy this script instance
+			} else {
+				renderBlueprint(position, rotation);
+			}
+		}
+	}
+
+	bool canBuild (out Vector3 position, out Quaternion rotation) {
+		position = Vector3.zero;
+		rotation = Quaternion.identity;
+
+		Ray groundRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+		RaycastHit hitInfo;
+		if (!Physics.Raycast(groundRay, out hitInfo, MAX_DISTANCE, 1 << LayerMask.NameToLayer("BuildSurface"))) {
+			return false;
+		}
+
+		if (Vector3.Dot(hitInfo.normal, Vector3.up) < MIN_NORMAL_ALIGNMENT) {
+			return false;
+		}
+
+		position = hitInfo.point;
+		rotation = transform.rotation;
+		return true;
+	}
+
+	void buildStructure (Vector3 position, Quaternion rotation) {
+		GameObject structure = Instantiate(Subject);
+		structure.transform.position = position;
+		structure.transform.rotation = rotation;
+
+		BelongsToTeam childBtt = structure.AddComponent<BelongsToTeam>();
+		childBtt.CopyFrom(btt);
+	}
+
+	void renderBlueprint (Vector3 position, Quaternion rotation) {
+		Matrix4x4 spawnlocationToLocal = Matrix4x4.TRS(position, rotation, Vector3.one);
+
+		foreach (Renderer mr in Subject.GetComponentsInChildren<Renderer>(true)) {
+			MeshFilter filter;
+			if ((filter = mr.GetComponent<MeshFilter>()) != null) {
+				Mesh mesh = filter.sharedMesh;
+				Matrix4x4 meshrendererToWorld = spawnlocationToLocal * mr.transform.localToWorldMatrix;
+				Graphics.DrawMesh(mesh, meshrendererToWorld, BlueprintMaterial, gameObject.layer, Camera.main);
+			}
+		}
+	}
+}
