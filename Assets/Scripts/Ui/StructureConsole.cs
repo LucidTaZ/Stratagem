bce4de172b7e48c81a1f3f62453f080f42c6d9f4
Thijs Zumbrink <tazaranie@gmail.com>
2016-08-06 10:44:52 +0200

    Reuse VirtualConsoleRaycaster for mouse movement

--- Assets/Scripts/Ui/StructureConsole.cs
+++ Assets/Scripts/Ui/StructureConsole.cs
@@ -3,29 +3,12 @@ using UnityEngine.UI;
 
 public class StructureConsole : MonoBehaviour {
 	public Button ButtonPrefab;
-	public GameObject CursorObject; // No prefab please
-
-	public float MaxDistance;
-	float sqrMaxDistance;
 
 	Canvas canvas;
-
-	const string LAYER_NAME = "World UI";
-
-	static Vector2 VirtualScreenDimension = new Vector2(800, 600);
 
 	void Start () {
 		canvas = GetComponentInChildren<Canvas>();
 		Debug.Assert(canvas != null);
-
-		if (canvas.GetComponent<BoxCollider>() == null) {
-			Debug.LogError("The canvas must have a BoxCollider to receive Physics Events");
-		}
-		if (canvas.gameObject.layer != LayerMask.NameToLayer(LAYER_NAME)) {
-			Debug.LogError("The canvas must have the World UI layer to receive raycasts");
-		}
-
-		sqrMaxDistance = MaxDistance * MaxDistance;
 
 		buildContents();
 	}
@@ -38,42 +21,6 @@ public class StructureConsole : MonoBehaviour {
 
 		Text label = button.GetComponentInChildren<Text>();
 		label.text = "Text set via script";
-	}
-
-	void Update () {
-		if (playerIsCloseEnough()) {
-			Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-			Vector2 virtualScreenPosition;
-			if (castRay(ray, out virtualScreenPosition)) {
-				moveMouseCursor(virtualScreenPosition);
-				// TODO: Disable crosshair once it exists
-			} else {
-				// TODO: Enable crosshair
-			}
-		}
-	}
-
-	bool playerIsCloseEnough () {
-		return (Camera.main.transform.position - canvas.transform.position).sqrMagnitude < sqrMaxDistance;
-	}
-
-	bool castRay (Ray ray, out Vector2 virtualScreenPosition) {
-		// Lower-left corner is (0,0), upper-right corder is (800,600)
-		RaycastHit hitInfo;
-		if (Physics.Raycast(ray, out hitInfo, MaxDistance, 1 << LayerMask.NameToLayer(LAYER_NAME))) {
-			Vector3 worldPosition = hitInfo.point;
-			Vector3 localPosition3D = canvas.transform.InverseTransformPoint(worldPosition);
-			Vector2 localPosition2D = localPosition3D;
-			virtualScreenPosition = VirtualScreenDimension / 2 + localPosition2D;
-			return true;
-		}
-		virtualScreenPosition = new Vector2();
-		return false;
-	}
-
-	void moveMouseCursor (Vector2 virtualScreenPosition) {
-		Vector2 unityLocalPosition = virtualScreenPosition - VirtualScreenDimension / 2; // Can be confusing: the editor shows something else because of the anchor point
-		CursorObject.GetComponent<RectTransform>().localPosition = unityLocalPosition;
 	}
 
 	void OnPurchaseButtonClicked () {
---
+++ Assets/Scripts/Ui/VirtualCursor.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class VirtualCursor : MonoBehaviour {
+	public GameObject CursorObject; // No prefab please
+
+	Canvas canvas;
+
+	void Start () {
+		canvas = GetComponentInChildren<Canvas>();
+		Debug.Assert(canvas != null);
+
+		if (canvas.GetComponent<BoxCollider>() == null) {
+			Debug.LogError("The canvas must have a BoxCollider to receive Physics Events");
+		}
+	}
+
+	void Update () {
+		Vector2 localPosition;
+		if (castRay(out localPosition)) {
+			moveMouseCursor(localPosition);
+			// TODO: Disable crosshair once it exists
+		} else {
+			// TODO: Enable crosshair
+		}
+	}
+
+	bool castRay (out Vector2 localPosition) {
+		// This method uses the configured VirtualConsoleRaycaster to see if we can interact with the virtual console.
+		// If so, it uses a conventional physics raycast to get the actual world point.
+
+		Vector2 screenPosition;
+		if (eventSystemHovered(out screenPosition)) {
+			Vector3 worldPosition = screenToWorldPosition(screenPosition);
+			Vector3 localPosition3D = canvas.transform.InverseTransformPoint(worldPosition);
+			localPosition = localPosition3D;
+			return true;
+		}
+
+		localPosition = new Vector2();
+		return false;
+	}
+
+	bool eventSystemHovered (out Vector2 screenPosition) {
+		List<RaycastResult> hits = new List<RaycastResult>();
+		PointerEventData pointer = new PointerEventData(EventSystem.current);
+		pointer.position = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+		VirtualConsoleRaycaster raycaster = GetComponentInChildren<VirtualConsoleRaycaster>();
+
+		raycaster.Raycast(pointer, hits);
+
+		if (hits.Count == 0) {
+			screenPosition = new Vector2();
+			return false;
+		}
+
+		screenPosition = hits[0].screenPosition;
+		return true;
+	}
+
+	Vector3 screenToWorldPosition (Vector2 screenPosition) {
+		Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+		RaycastHit hitInfo;
+		if (!Physics.Raycast(ray, out hitInfo, Mathf.Infinity, 1 << canvas.gameObject.layer)) {
+			Debug.LogError("Screenposition has no place in world");
+		}
+		return hitInfo.point;
+	}
+
+	void moveMouseCursor (Vector2 localPosition) {
+		CursorObject.GetComponent<RectTransform>().localPosition = localPosition;
+	}
+}
