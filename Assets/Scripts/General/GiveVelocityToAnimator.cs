01a121f856b40ab1e160ee8f6a3bf2468d561168
Thijs Zumbrink <tazaranie@gmail.com>
2016-08-13 13:17:23 +0200

    Add worker model and animation

---
+++ Assets/Scripts/General/GiveVelocityToAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GiveVelocityToAnimator : MonoBehaviour {
+	Vector3 previousPosition;
+
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		Debug.Assert(anim != null);
+	}
+
+	void Update () {
+		float ds = Vector3.Dot(transform.position - previousPosition, -transform.forward); // Note: transform.forward is turned around because I modeled the units the wrong way around...
+		previousPosition = transform.position;
+
+		float v = ds / Time.deltaTime;
+		bool isMoving = Mathf.Abs(v) > Mathf.Epsilon;
+
+		anim.SetFloat("velocity", v);
+		anim.SetBool("isMoving", isMoving);
+	}
+}
