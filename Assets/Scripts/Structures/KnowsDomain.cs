866cf7d1ea89e5e485f7f5c1aed6b0a5a0444a8c
Thijs Zumbrink <tazaranie@gmail.com>
2016-08-27 16:20:06 +0200

    Show range indicator of structure to place

--- Assets/Scripts/Structures/KnowsDomain.cs
+++ Assets/Scripts/Structures/KnowsDomain.cs
@@ -23,17 +23,42 @@ public class KnowsDomain : MonoBehaviour {
 		initializeRangeIndicator();
 	}
 
+	public MeshAndTransform GetRangeIndicatorBeforeConstruction () {
+		// In this method, the structure is not actually built yet, so we miss some information such as team
+		Renderer mr = RangeIndicator.GetComponent<Renderer>();
+		Debug.Assert(mr != null);
+		MeshFilter filter = mr.GetComponent<MeshFilter>();
+		Debug.Assert(filter != null);
+
+		Mesh mesh = filter.sharedMesh;
+		Matrix4x4 meshrendererToWorld = Matrix4x4.TRS(
+			Domain.Center,
+			Quaternion.identity,
+			new Vector3(Domain.Range * 2, RangeIndicator.transform.localScale.y, Domain.Range * 2)
+		);
+
+		return new MeshAndTransform(mesh, meshrendererToWorld);
+	}
+
 	void initializeRangeIndicator () {
 		// TODO: Maybe drawing a line is better than having a big cylinder.
 		// http://answers.unity3d.com/questions/31949/position-a-circle-under-my-character.html
 		// http://docs.unity3d.com/Manual/class-Projector.html
 		// http://docs.unity3d.com/Manual/class-LineRenderer.html
+		initializeRangeIndicatorTransform();
+		initializeRangeIndicatorColor();
+	}
+
+	void initializeRangeIndicatorTransform () {
 		RangeIndicator.transform.position = Domain.Center;
 		RangeIndicator.transform.localScale = new Vector3(
 			Domain.Range * 2,
 			RangeIndicator.transform.localScale.y,
 			Domain.Range * 2
 		);
+	}
+
+	void initializeRangeIndicatorColor () {
 		Material material = RangeIndicator.GetComponent<MeshRenderer>().material;
 		setColorRGBOnly(material, "_Color", Domain.FriendlyTeam.color);
 		material.SetColor("_EmissionColor", Color.Lerp(Color.black, Domain.FriendlyTeam.color, 0.1f));
