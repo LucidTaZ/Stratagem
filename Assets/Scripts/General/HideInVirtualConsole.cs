7f479f629c874ce530e3c56c271d44c48c170172
Thijs Zumbrink <tazaranie@gmail.com>
2016-08-28 12:45:33 +0200

    Hide local weapon while in virtual console

---
+++ Assets/Scripts/General/HideInVirtualConsole.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class HideInVirtualConsole : MonoBehaviour {
+	Renderer[] renderers;
+	PlayerState playerState;
+
+	bool currentlyHiding;
+
+	void Awake () {
+		renderers = GetComponentsInChildren<Renderer>();
+		playerState = GameObject.FindGameObjectWithTag("PlayerState").GetComponent<PlayerState>();
+	}
+
+	void Start () {
+		setDisplay();
+	}
+
+	void Update () {
+		toggleDisplayIfNeeded();
+	}
+
+	void toggleDisplayIfNeeded () {
+		if (currentlyHiding != playerState.IsInVirtualConsole) {
+			currentlyHiding = playerState.IsInVirtualConsole;
+			setDisplay();
+		}
+	}
+
+	void setDisplay () {
+		foreach (Renderer renderer in renderers) {
+			renderer.enabled = !currentlyHiding;
+		}
+	}
+}
