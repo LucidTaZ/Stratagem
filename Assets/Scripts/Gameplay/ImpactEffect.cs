44dfba13afac91ea026b5d0e21c7cc909edfa993
Thijs Zumbrink <tazaranie@gmail.com>
2016-09-05 14:11:55 +0200

    Add impact effects

---
+++ Assets/Scripts/Gameplay/ImpactEffect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ImpactEffect : MonoBehaviour {
+	public GameObject EffectToSpawn;
+
+	void OnCollisionEnter (Collision collision) {
+		ContactPoint hitpoint = collision.contacts[0];
+		createEffect(hitpoint.point, hitpoint.normal, collision.transform);
+		Destroy(this);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		// We need this method because units have triggers, not colliders
+		if (other.GetComponent<Hitpoints>() != null) {
+			Rigidbody rb = GetComponent<Rigidbody>();
+			Vector3 hitpoint = other.ClosestPointOnBounds(transform.position);
+			createEffect(hitpoint, -rb.velocity.normalized, other.transform);
+			Destroy(this);
+		}
+	}
+
+	void createEffect (Vector3 position, Vector3 normal, Transform parent) {
+		GameObject instance = Instantiate(EffectToSpawn);
+		instance.transform.position = position;
+		instance.transform.rotation = Quaternion.LookRotation(-normal);
+
+		// Move with the hit object and disappear when it disappears
+		instance.transform.parent = parent;
+
+		// Correct the scale back to "normal" size, regardless of the parent object scale
+		instance.transform.localScale = new Vector3(
+			1.0f / parent.localScale.x,
+			1.0f / parent.localScale.y,
+			1.0f / parent.localScale.z
+		);
+	}
+}
