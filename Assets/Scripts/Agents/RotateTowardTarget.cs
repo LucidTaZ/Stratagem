be2ca772d3759dd3ae43cf23969a806c89d55a7f
Thijs Zumbrink <tazaranie@gmail.com>
2016-09-07 18:59:03 +0200

    Tweak animation to prepare for additions

--- Assets/Scripts/Agents/RotateTowardTarget.cs
+++ Assets/Scripts/Agents/RotateTowardTarget.cs
@@ -1,28 +1,27 @@
 ﻿using UnityEngine;
 
 public class RotateTowardTarget : MonoBehaviour {
-	public Transform Bone;
-
 	public float SlerpSpeed;
 
 	FindTarget findTarget;
 	Quaternion initialRotation;
 
 	void Awake () {
-		findTarget = GetComponent<FindTarget>();
+		findTarget = GetComponentInParent<FindTarget>();
 		Debug.Assert(findTarget != null);
 
-		initialRotation = Bone.rotation;
+		initialRotation = transform.rotation;
 	}
 
 	void Update () {
 		if (findTarget.HasTarget()) {
 			Vector3 targetPosition = findTarget.CurrentTarget.transform.position;
-			Vector3 targetDirection = targetPosition - Bone.position;
+			Vector3 targetDirection = targetPosition - transform.position;
+
 			targetDirection.y = 0; // Ensure we only rotate around world y
 
-			Bone.rotation = Quaternion.Slerp(
-				Bone.rotation,
+			transform.rotation = Quaternion.Slerp(
+				transform.rotation,
 				Quaternion.LookRotation(targetDirection, Vector3.up) * initialRotation,
 				SlerpSpeed * Time.deltaTime
 			);
